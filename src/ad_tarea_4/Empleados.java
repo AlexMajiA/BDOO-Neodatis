40a0aba0931b254ffd21d64967c8fa24d64e75f5
Alejandro Martínez <amj.parrales@gmail.com>
2024-12-30 14:03:16 +0100

    Inserción de elementos hecha.

--- src/ad_tarea_4/Empleados.java
+++ src/ad_tarea_4/Empleados.java
@@ -4,6 +4,12 @@
  */
 package ad_tarea_4;
 
+import java.util.HashSet;
+import java.util.Set;
+import org.neodatis.odb.ODB;
+import org.neodatis.odb.ODBFactory;
+import org.neodatis.odb.Objects;
+
 /**
  *
  * @author amjpa
@@ -74,6 +80,31 @@ public class Empleados {
     }
     
     
-    
+    public class EliminarDuplicados{
+            public static void main(String[] args) {
+                
+                String rutaBd = System.getProperty("user.dir") + "gestionLaboral";
+                
+                ODB odb = ODBFactory.open(rutaBd);
+                
+                //Recupero los objetos de la clase Empleado.
+                Objects<Empleados> empleados = odb.getObjects(Empleados.class);
+                
+                //Uso un set para identificar los duplicados.
+                Set<String> identificadorUnico = new HashSet<>();
+                int eliminados = 0;
+                
+                while(empleados.hasNext()){
+                    Empleados empleado = empleados.next();
+                
+                //Uso atributo único como criterio para identificar duplicados.
+                    if (!identificadorUnico.add(String.valueOf(empleado.codEmpleado))) {
+                        odb.delete(empleado); //Elimino el duplicado.
+                        eliminados++;
+                    }
+                System.out.println("Se han eliminado " + eliminados + " registros duplicados.");
+                }
+            }
+        }
     
 }
--- src/ad_tarea_4/crearBDOO.java
+++ src/ad_tarea_4/crearBDOO.java
@@ -5,6 +5,7 @@
 package ad_tarea_4;
 
 import java.io.File;
+import org.neodatis.odb.ODB;
 import org.neodatis.odb.ODBFactory;
 
 /**
@@ -18,9 +19,30 @@ public class crearBDOO {
      */
     public static void main(String[] args) {
       
-        String rutaDesktop = System.getProperty("user.home") + File.separator + "Desktop";
-        ODBFactory.open(rutaDesktop + File.separator + "gestionLaboral");
-  
+        //Creo la ruta en la carpeta del proyecto.
+        String rutaDesktop = System.getProperty("user.dir");
+        
+        //Abro conexión con la BDOO
+        ODB odb = ODBFactory.open(rutaDesktop + File.separator + "gestionLaboral");
+        
+        System.out.println("Conexión a la base de datos creada en: " + rutaDesktop + File.separator + "gestionLaboral.neodatis");
+            
+        
+        Empleados emp1 = new Empleados(1, "Alejandro", "Martinez", "Desarrollador", 1750);
+        Empleados emp2 = new Empleados(2, "Manuel", "Jimenez", "Contable", 1200);
+        Empleados emp3 = new Empleados(3, "Silvia", "Marzo", "Desarrollador", 1600);
+        Empleados emp4 = new Empleados(4, "Migue", "Cerezo", "Analista", 1500);
+        Empleados emp5 = new Empleados(5, "Sara", "Toro", "Gerente", 1800);
+        
+        //Almaceno los objetos.
+        odb.store(emp1);
+        odb.store(emp2);
+        odb.store(emp3);
+        odb.store(emp4);
+        odb.store(emp5);
+        
+        //Cierro la conexión.
+        odb.close();
         
         
     }
